4df1d6dcc12e58044b111533879ae0392ee8247e
snaztoz <hafidhmn@gmail.com>
2021-06-09 11:33:00 +0700

    Membuat API untuk mengambil daftar universitas.

--- api/api.py
+++ api/api.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+import sqlite3
 
+conn = sqlite3.connect('data-snmptn.db')
 app = FastAPI()
 
 
 @app.get('/')
 async def index():
     return {'message': 'It Works!'}
+
+
+@app.get('/list-universitas')
+async def get_universitas():
+    cursor = conn.cursor()
+
+    result = []
+    for row in cursor.execute('SELECT DISTINCT universitas FROM Jurusan'):
+        result.append(row[0])
+
+    cursor.close()
+    return {'list-universitas': result}
